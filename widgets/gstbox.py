07752e6958f6f44ec1101d100224cca8c5d3a3a9
jonian <jonian@hardpixel.eu>
2017-07-17 11:53:50 +0300

    remove unsused require

--- widgets/gstbox.py
+++ widgets/gstbox.py
@@ -3,7 +3,6 @@ import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('GLib', '2.0')
 gi.require_version('Gst', '1.0')
-gi.require_version('GstVideo', '1.0')
 
 from gi.repository import Gtk, GLib, Gst, GObject
 
