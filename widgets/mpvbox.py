d886e0484cbec814e2b72fb8164d3eba71a5e308
Jonian Guveli <jonian@hardpixel.eu>
2018-10-06 13:06:17 +0300

    add player widget with mpv backend

---
+++ widgets/mpvbox.py
@@ -0,0 +1,51 @@
+import gi
+import mpv
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk, GObject
+from helpers.gtk import add_widget_class
+
+
+class MpvBox(Gtk.Box):
+
+  __gtype_name__ = 'MpvBox'
+
+  callback = GObject.property(type=object, flags=GObject.PARAM_READWRITE)
+
+  def __init__(self, *args, **kwargs):
+    Gtk.Box.__init__(self, *args, **kwargs)
+
+    self.canvas = Gtk.DrawingArea()
+    self.pack_start(self.canvas, True, True, 0)
+
+    self.player = mpv.MPV(ytdl=True)
+    self.canvas.connect('realize', self.on_canvas_realize)
+    self.canvas.connect('draw', self.on_canvas_draw)
+
+    add_widget_class(self, 'player-video')
+
+  def open(self, url):
+    self.player.play(url)
+
+  def play(self):
+    self.player._set_property('pause', False)
+    self.callback('PLAYING')
+
+  def pause(self):
+    self.player._set_property('pause', True)
+    self.callback('PAUSED')
+
+  def stop(self):
+    self.player.command('stop')
+    self.callback('STOPPED')
+
+  def set_volume(self, volume):
+    self.player._set_property('volume', volume)
+
+  def on_canvas_realize(self, widget):
+    self.player.wid = widget.get_property('window').get_xid()
+
+  def on_canvas_draw(self, widget, cr):
+    cr.set_source_rgb(0.0, 0.0, 0.0)
+    cr.paint()
